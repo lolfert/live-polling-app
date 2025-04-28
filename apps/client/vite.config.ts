579ca8ad08602d78f2e2ab04f5c636c0c39e8f4b
Luke Olfert <laolfert@gmail.com>
2025-04-28 11:48:02 -0500

    Updated environment variable handling in Vite config.

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,28 +1,40 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite';
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-import path from "path"
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+export default defineConfig(({ mode }) => {
 
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    port: 3000,
-    proxy: {
-      // Proxy API requests to the backend server
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        // rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
-})
+        const root = path.resolve(__dirname, '../..');
+
+        process.env = {
+                ...process.env,
+                ...loadEnv(mode, root)
+        };
+
+        if (!process.env.VITE_API_URL) {
+                throw new Error('VITE_API_URL is not defined in .env file');
+        }
+
+        return {
+                plugins: [
+                        react(),
+                        tailwindcss(),
+                ],
+                resolve: {
+                        alias: {
+                                '@': path.resolve(__dirname, './src'),
+                        },
+                },
+                server: {
+                        port: 3000,
+                        proxy: {
+                                '/api': {
+                                        target: process.env.VITE_API_URL,
+                                        changeOrigin: true,
+                                },
+                        },
+                },
+        };
+
+});
